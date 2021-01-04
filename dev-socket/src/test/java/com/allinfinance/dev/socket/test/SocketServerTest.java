df3e47e8fabd7dbed0f6772df7b1cc32a4d3f967
zhangyong <zhangyong@allinfinance.com>
2021-01-04 20:41:33 +0800

    整合dubbo2.7.8并贴出实例，部分优化

--- dev-socket/src/test/java/com/allinfinance/dev/socket/test/SocketServerTest.java
+++ dev-socket/src/test/java/com/allinfinance/dev/socket/test/SocketServerTest.java
@@ -2,21 +2,23 @@ package com.allinfinance.dev.socket.test;
 
 import com.allinfinance.dev.socket.MinaApplication;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author 张勇
  * @description
  * @date 2020/11/30 21:01
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:application-context-test.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("classpath:application-context-test.xml")
 public class SocketServerTest {
     @Test
     public void test() {
+        MinaApplication.run(SocketServerTest.class);
+    }
+
+    public static void main(String[] args) {
+        new ClassPathXmlApplicationContext("classpath:application-context-test.xml");
         MinaApplication.run(SocketServerTest.class);
     }
 }
