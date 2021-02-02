d41b06fc7f4c6593a85ea88052fef94eb0aca9db
zhangyong <zhangyong@allinfinance.com>
2021-02-02 21:08:21 +0800

    HttpClientService修复

---
+++ dev-example/dev-redis-example/src/main/java/com/allinfinance/dev/example/redis/RedisExampleApplication.java
@@ -0,0 +1,11 @@
+package com.allinfinance.dev.example.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExampleApplication.class, args);
+    }
+}
