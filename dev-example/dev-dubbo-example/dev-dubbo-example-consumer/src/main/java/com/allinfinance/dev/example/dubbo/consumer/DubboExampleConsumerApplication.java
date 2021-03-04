eda67077179faa99c5755dc21e288ef49eb67da5
zhangyong <zhangyong@allinfinance.com>
2021-03-04 13:21:59 +0800

    1、配置文件统一加载模块 2、差异化平台测试

--- dev-example/dev-dubbo-example/dev-dubbo-example-consumer/src/main/java/com/allinfinance/dev/example/dubbo/consumer/DubboExampleConsumerApplication.java
+++ dev-example/dev-dubbo-example/dev-dubbo-example-consumer/src/main/java/com/allinfinance/dev/example/dubbo/consumer/DubboExampleConsumerApplication.java
@@ -1,7 +1,7 @@
 package com.allinfinance.dev.example.dubbo.consumer;
 
-import com.allinfinance.dev.dev.example.dubbo.ExampleInterface;
-import com.allinfinance.dev.dubbo.config.DynamicDubboService;
+import com.allinfinance.dev.mrp.param.RequestParams;
+import com.allinfinance.dev.mrp.service.GenericMqRpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,32 +11,54 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(scanBasePackages = "com.allinfinance")
-@ImportResource(locations = {"classpath:example-dubbo-consumer-config.xml"})
+@ImportResource(locations = {"classpath:example-dubbo-consumer-config.xml", "classpath:example-rabbitmq-producer-config.xml"})
 public class DubboExampleConsumerApplication {
     private static final Logger logger = LoggerFactory.getLogger(DubboExampleConsumerApplication.class);
 
-    @Autowired
-    private DynamicDubboService dubboService;
+//    @Autowired
+//    private DynamicDubboService dubboService;
+//
+//    @Autowired
+//    private ExampleInterface exampleInterface;
 
     @Autowired
-    private ExampleInterface exampleInterface;
+    private GenericMqRpcService<String, RequestParams> stringGenericMqRpcService;
 
     public static void main(String[] args) {
         SpringApplication.run(DubboExampleConsumerApplication.class, args);
     }
 
-    @Bean
-    public void testDynamicDubboService() {
-        try {
-            exampleInterface = dubboService.getDubboService("0001", ExampleInterface.class);
-        } catch (Exception e) {
-            logger.error("动态获取dubbo服务异常!", e);
-        }
-        logger.info("consumer发送内容:{}", exampleInterface.hello("test dynamicDubboService"));
-    }
+//    @Bean
+//    public void testDynamicDubboService() {
+//        try {
+//            exampleInterface = dubboService.getDubboService("0001", ExampleInterface.class);
+//        } catch (Exception e) {
+//            logger.error("动态获取dubbo服务异常!", e);
+//        }
+//        logger.info("consumer发送内容:{}", exampleInterface.hello("test dynamicDubboService"));
+//    }
+//
+//    @Bean
+//    public void testAutowired() {
+//        logger.info("consumer发送内容:{}", exampleInterface.hello("test autowired"));
+//    }
+
+//    @Bean
+//    public void testGenericRpc() {
+//        RequestParams requestParams = new RequestParams();
+//        requestParams.setRpcInterface("com.allinfinance.dev.dev.example.dubbo.ExampleInterface");
+//        requestParams.setRpcMethod("hello");
+//        logger.info("consumer发送内容:{}", stringGenericMqRpcService.genericInvoke("test generic", requestParams));
+//    }
 
     @Bean
-    public void testAutowired() {
-        logger.info("consumer发送内容:{}", exampleInterface.hello("test autowired"));
+    public void testGenericMq() {
+        RequestParams requestParams = new RequestParams();
+        requestParams.setRpcInterface("com.allinfinance.dev.dev.example.dubbo.ExampleInterface");
+        requestParams.setRpcMethod("hello");
+        requestParams.setExchangeName("fanout-mq-exchange");
+        requestParams.setRoutingKey("test-routing-key");
+        logger.info("consumer发送内容:{}", stringGenericMqRpcService.genericInvoke(requestParams, requestParams));
     }
+
 }
--- dev-mq-rpc-portal/src/main/java/com/allinfinance/dev/mrp/service/GenericMqRpcService.java
+++ dev-mq-rpc-portal/src/main/java/com/allinfinance/dev/mrp/service/GenericMqRpcService.java
@@ -1,15 +1,15 @@
 package com.allinfinance.dev.mrp.service;
 
 import com.allinfinance.dev.core.loader.SpringConfigTool;
+import com.allinfinance.dev.mrp.param.GenericReponse;
 import com.allinfinance.dev.mrp.param.RequestParams;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 /**
@@ -19,35 +19,42 @@ import java.util.NoSuchElementException;
 @Component
 public class GenericMqRpcService<T, K> {
 
+    @Value("${dev.mrp.switch}")
+    private String mrpSwitch;
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    public Map<String, Object> genericInvoke(K req, RequestParams params) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-        Map<String, Object> result = new HashMap<>();
-        switch (params.getMrpSwitch()) {
+    public GenericReponse<T> genericInvoke(K req, RequestParams params) {
+        GenericReponse<T> response = new GenericReponse<>(Boolean.TRUE);
+        switch (mrpSwitch) {
             case RequestParams.MQ_SWITCH:
-                rabbitTemplate.convertAndSend(params.getExchangeName(), params.getRoutingKey(), req);
+                try {
+                    rabbitTemplate.convertAndSend(params.getExchangeName(), params.getRoutingKey(), req);
+                } catch (Exception mqException) {
+                    log.error("调用MQ发布消息异常!", mqException);
+                    response.setRespStatus(Boolean.FALSE);
+                }
                 break;
             case RequestParams.RPC_SWITCH:
                 /**
-                 *
+                 * 1、根据接口类获取refference bean
+                 * 2、根据rpc方法，请求类型，请求体反射调用
                  */
                 try {
                     Object beanByClassName = SpringConfigTool.getBeanByClassName(params.getRpcInterface());
                     T invokeResp = (T) Class.forName(params.getRpcInterface())
                             .getDeclaredMethod(params.getRpcMethod(), req.getClass())
                             .invoke(beanByClassName, req);
-                    result.put("rpcInvokeData", invokeResp);
+                    response.setRpcInvokeData(invokeResp);
                 } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
                     log.error("rpc 调用失败!", e);
-                    throw e;
+                    response.setRespStatus(Boolean.FALSE);
                 }
                 break;
             default:
-                log.error("开关配置有误：{}!", params.getMrpSwitch());
+                log.error("开关配置有误：{}!", mrpSwitch);
                 throw new NoSuchElementException("开关配置有误!只允许为【MQ/RPC】");
         }
-        result.put("status", Boolean.TRUE);
-        return result;
+        return response;
     }
 }
