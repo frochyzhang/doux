d895c8cb42b2f12e956434290ca5b154825d0b1e
wanglong <wanglong@allinfinance.com>
2021-05-18 15:01:20 +0800

    role auth controller update

--- dev-ccs/src/main/java/com/allinfinance/dev/ccs/controller/RoleAuthController.java
+++ dev-ccs/src/main/java/com/allinfinance/dev/ccs/controller/RoleAuthController.java
@@ -6,6 +6,7 @@ import com.allinfinance.dev.ccs.dal.model.TblRoleAuth;
 import com.allinfinance.dev.ccs.dal.model.TblRoleAuthKey;
 import com.allinfinance.dev.ccs.dal.service.TblAuthService;
 import com.allinfinance.dev.ccs.dal.service.TblRoleAuthService;
+import com.allinfinance.dev.ccs.result.Result;
 import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
  * @create: 2021-05-14 13:56
  */
 @RestController
-@RequestMapping("/platform")
+@RequestMapping("/platform/roleAuths")
 public class RoleAuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(RoleAuthController.class);
@@ -28,35 +29,58 @@ public class RoleAuthController {
     private TblRoleAuthService tblRoleAuthService;
 
     //分页查询权限
-    @RequestMapping(path = "/roleAuths",method = RequestMethod.GET)
-    public PageInfo<TblRoleAuth> selectUsers( @RequestParam("roleId") int roleId, @RequestParam("authId") int authId,
-                                          @RequestParam("pageNo") int pageNo, @RequestParam("pageSize") int pageSize){
+    @RequestMapping(method = RequestMethod.GET)
+    public Result selectRoleAuths(@RequestParam("roleId") int roleId, @RequestParam("authId") int authId,
+                              @RequestParam("pageNo") int pageNo, @RequestParam("pageSize") int pageSize){
         logger.info("接受到的参数:roleId-{},authId-{},pageNo-{},pageSize-{}",roleId,authId,pageNo,pageSize);
-        PageInfo<TblRoleAuth> roleAuths = tblRoleAuthService.pageSelectRoleAuths(pageNo,pageSize);
-        logger.info("查询到的角色权限列表: {}",roleAuths);
-        return roleAuths;
+        PageInfo<TblRoleAuth> roleAuths;
+        try {
+            roleAuths = tblRoleAuthService.pageSelectRoleAuths(pageNo,pageSize);
+        }catch (Exception e){
+            logger.error("分页查询角色权限发生异常",e);
+            return Result.failure();
+        }
+        return Result.success(roleAuths);
     }
 
 
     //修改角色
-    @RequestMapping(path = "/roles",method = RequestMethod.PUT)
-    public boolean modifyUser(@RequestBody TblRoleAuth tblRoleAuth){
+    @RequestMapping(method = RequestMethod.PUT)
+    public Result modifyRoleAuth(@RequestBody TblRoleAuth tblRoleAuth){
         logger.info("接收到的请求参数:{}",tblRoleAuth);
-        int result = tblRoleAuthService.updateByPrimaryKey(tblRoleAuth);
-        logger.info("result: {}",result);
-        return result == 1;
+        int result;
+        try {
+            result = tblRoleAuthService.updateByPrimaryKey(tblRoleAuth);
+        }catch (Exception e){
+            return Result.failure();
+        }
+        if (result == 1){
+            return Result.success();
+        }else {
+            return Result.failure();
+        }
     }
 
 
     //新增角色权限
-    @RequestMapping(path = "/roleAuths",method = RequestMethod.POST)
-    public boolean createAuth(@RequestBody TblRoleAuth tblRoleAuth){
+    @RequestMapping(method = RequestMethod.POST)
+    public Result createRoleAuth(@RequestBody TblRoleAuth tblRoleAuth){
         logger.info("将新增的角色权限: {}",tblRoleAuth);
-        int result = tblRoleAuthService.insertSelective(tblRoleAuth);
-        logger.info("新增结果: {}",result);
-        return result == 1;
-    }
 
+        int result;
+        try {
+            result = tblRoleAuthService.insertSelective(tblRoleAuth);
+        }catch (Exception e){
+            logger.error("新增角色权限发生异常",e);
+            return Result.failure();
+        }
+        logger.info("新增结果: {}",result);
+        if (result == 1){
+            return Result.success();
+        }else {
+            return Result.failure();
+        }
+    }
 
 
 }
