873b370ae9e1598fc22d5b851b0b98a1576e0283
zhangyong <zhangyong@allinfinance.com>
2021-07-24 20:21:34 +0800

    delete MapperScanner

--- dev-ccs/src/main/java/com/allinfinance/dev/ccs/CCSApplication.java
+++ dev-ccs/src/main/java/com/allinfinance/dev/ccs/CCSApplication.java
@@ -1,17 +1,17 @@
 package com.allinfinance.dev.ccs;
 
 import com.allinfinance.dev.ccs.content.RSAKeyProperties;
-import com.allinfinance.dev.dal.mybatis.MapperScanner;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication(scanBasePackages = "com.allinfinance")
-@MapperScanner(basePackages = "com.allinfinance.dev.ccs.dal.mapper",sqlSessionFactoryRef = "sqlSessionFactory")
+@MapperScan(basePackages = "com.allinfinance.dev.ccs.dal.mapper", sqlSessionFactoryRef = "sqlSessionFactory")
 @EnableConfigurationProperties({RSAKeyProperties.class})
 public class CCSApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CCSApplication.class,args);
+        SpringApplication.run(CCSApplication.class, args);
     }
 }
