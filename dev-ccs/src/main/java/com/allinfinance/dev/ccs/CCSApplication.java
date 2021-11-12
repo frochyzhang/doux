a65681e4044769bdb44440897f0ec4a2c7c717b5
张勇 <zhangyong@allinfiannce.com>
2021-11-12 09:39:26 +0800

    改造？？？

--- dev-ccs/src/main/java/com/allinfinance/dev/ccs/CCSApplication.java
+++ dev-ccs/src/main/java/com/allinfinance/dev/ccs/CCSApplication.java
@@ -1,14 +1,14 @@
 package com.allinfinance.dev.ccs;
 
 import com.allinfinance.dev.ccs.content.RSAKeyProperties;
-import com.allinfinance.dev.dal.mybatis.MapperScanner;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication(scanBasePackages = "com.allinfinance")
-@MapperScanner(basePackages = "com.allinfinance.dev.ccs.dal.mapper",sqlSessionFactoryRef = "sqlSessionFactory")
+@MapperScan(basePackages = "com.allinfinance.dev.ccs.dal.mapper")
 @EnableConfigurationProperties({RSAKeyProperties.class})
 public class CCSApplication {
     public static void main(String[] args) {
