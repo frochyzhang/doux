fc4701c178610c538a4c336bda760f4eb493d9cc
liqf <liqf@allinfinance.com>
2022-01-10 16:16:11 +0800

    '去除配置文件中OTP开启控制'

--- dev-ccs/src/main/java/com/allinfinance/dev/ccs/dal/service/impl/LoginServiceIpml.java
+++ dev-ccs/src/main/java/com/allinfinance/dev/ccs/dal/service/impl/LoginServiceIpml.java
@@ -47,9 +47,6 @@ public class LoginServiceIpml implements LoginService {
 
     private static String DEST_PATH;
 
-    @Value("${optValue}")
-    private boolean optValue;
-
     @Value("${qrCode.path:/home/aos/qrcode/}")
     public void setDesePath(String desePath) {
         this.DEST_PATH = desePath;
@@ -64,19 +61,17 @@ public class LoginServiceIpml implements LoginService {
             logger.debug("OTP验证码不能为空！");
             return Result.failure(ResultCodeEnum.PARAM_IS_INVALID);
         }
-        if (optValue) {
-            try {
-                String secret = currentUser.getReservedField2();
-                int code = Integer.valueOf(checkPassVo.getCheckCode());
-                boolean validate = GoogleAuthenticator.validateCurrentNumber(secret, code, -1);
-                if (!validate) {
-                    logger.debug("OTP验证码无效！");
-                    return Result.failure(ResultCodeEnum.USER_ACCOUNT_ODEERROR);
-                }
-            } catch (GeneralSecurityException e) {
-                logger.error("OTP验证码验证异常！", e);
-                return Result.failure(ResultCodeEnum.GENERIC_EXCEPTION);
+        try {
+            String secret = currentUser.getReservedField2();
+            int code = Integer.valueOf(checkPassVo.getCheckCode());
+            boolean validate = GoogleAuthenticator.validateCurrentNumber(secret, code, -1);
+            if (!validate) {
+                logger.debug("OTP验证码无效！");
+                return Result.failure(ResultCodeEnum.USER_ACCOUNT_ODEERROR);
             }
+        } catch (GeneralSecurityException e) {
+            logger.error("OTP验证码验证异常！", e);
+            return Result.failure(ResultCodeEnum.GENERIC_EXCEPTION);
         }
         currentUser.setReservedField1(AosContent.IS_BIND);
         currentUser.setUpdateTime(new Date());
