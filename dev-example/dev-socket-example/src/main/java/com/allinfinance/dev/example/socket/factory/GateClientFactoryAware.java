65de25a29510104f1449f9c28007eb626149620a
Administrator <admin@example.com>
2022-02-07 12:38:06 +0800

    :sparkles: 网关重启后拉起服务订阅

--- dev-example/dev-socket-example/src/main/java/com/allinfinance/dev/example/socket/factory/GateClientFactoryAware.java
+++ dev-example/dev-socket-example/src/main/java/com/allinfinance/dev/example/socket/factory/GateClientFactoryAware.java
@@ -1,7 +1,5 @@
 package com.allinfinance.dev.example.socket.factory;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.io.LineHandler;
 import com.alipay.sofa.rpc.boot.runtime.param.BoltBindingParam;
 import com.alipay.sofa.rpc.core.exception.SofaRouteException;
 import com.alipay.sofa.runtime.api.aware.ClientFactoryAware;
@@ -11,13 +9,14 @@ import com.alipay.sofa.runtime.api.client.param.BindingParam;
 import com.alipay.sofa.runtime.api.client.param.ReferenceParam;
 import com.allinfinance.dev.rpc.scaffold.api.ProcessService;
 import com.google.gson.Gson;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author <a href="mailto:frochyzhang@gmail.com>frochyZhang</a>
@@ -37,16 +36,36 @@ public class GateClientFactoryAware implements ClientFactoryAware {
 
         this.referenceClient = clientFactory.getClient(ReferenceClient.class);
 
-        File file = new File(AppProcessFactory.CACHE_DATA_NAME);
-        if (file.exists()) {
-            FileUtil.readLines(file, StandardCharsets.UTF_8, (LineHandler) s -> {
-                AppProcessFactory.AppRegisterProperties appRegisterProperties = new Gson().fromJson(s, AppProcessFactory.AppRegisterProperties.class);
-                if (registerConsumer(appRegisterProperties.getUniqueId())) {
-                    logger.info("[ {} ]应用注册成功!", appRegisterProperties.getUniqueId());
-                }
 
-            });
-        }
+        String url = "http://10.100.79.102:8848/nacos/v1/ns/service/list?pageNo=1&pageSize=10&serviceNameParam=&groupNameParam=&namespaceId=iasp-zy";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .get()//默认就是GET请求，可以不写
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                logger.error("onFailure: {}", call, e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String resp = response.body().string();
+                ServiceListDTO serviceListDTO = new Gson().fromJson(resp, ServiceListDTO.class);
+                serviceListDTO.getDoms().stream()
+                        .filter(service -> service.contains(ProcessService.class.getName()))
+                        .forEach(service -> {
+                            String uniqueId = service.split(":")[1];
+                            if (registerConsumer(uniqueId)) {
+                                logger.info("[ {} ]应用注册成功!", uniqueId);
+                            }
+                        });
+            }
+        });
+
+
     }
 
     public Boolean registerConsumer(String uniqueId) {
@@ -80,6 +99,35 @@ public class GateClientFactoryAware implements ClientFactoryAware {
             logger.warn("[ {} ]已掉线,移除订阅!", uniqueId);
             referenceClient.removeReference(referenceParam);
             return Boolean.FALSE;
+        }
+    }
+
+    public static class ServiceListDTO {
+        private List<String> doms;
+        private Integer count;
+
+        public List<String> getDoms() {
+            return doms;
+        }
+
+        public void setDoms(List<String> doms) {
+            this.doms = doms;
+        }
+
+        public Integer getCount() {
+            return count;
+        }
+
+        public void setCount(Integer count) {
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return "ServiceListDTO{" +
+                    "doms=" + doms +
+                    ", count=" + count +
+                    '}';
         }
     }
 }
