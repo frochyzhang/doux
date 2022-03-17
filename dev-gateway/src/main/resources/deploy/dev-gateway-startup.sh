d3617c245a2bc2ca74239d2f4a8bebcf76be39e0
qipeng <qipeng@allinfinance.com>
2022-03-17 14:11:39 +0800

    网关+ccp整合skywalking

--- dev-gateway/src/main/resources/deploy/dev-gateway-startup.sh
+++ dev-gateway/src/main/resources/deploy/dev-gateway-startup.sh
@@ -1,7 +1,7 @@
 #Active Profile(YAML)
 ACTIVE_PROFILE="$1"
 if [ -z "$ACTIVE_PROFILE" ]; then
-  echo "Usage: sit|uat|prod, default set to prod" 1>&2
+  echo "Usage: sit|uat|prod, otherwise exit!" 1>&2
   exit 1
 fi
 # JVM Parameters and Spring boot initialization parameters
@@ -15,6 +15,21 @@ SHELL_SCRIPT_FILE_NAME=$(basename -- "$0")
 #SHELL_SCRIPT_FILE_NAME_WITHOUT_EXT="${SHELL_SCRIPT_FILE_NAME%.sh}"
 # App name after removing start/stop strings like "yaml-validator"
 APP_NAME=${SHELL_SCRIPT_FILE_NAME%-startup.sh}
+
+# skywalking相关配置
+SKY_WALKING_BACKEND_ADDRESS=""
+if [ $ACTIVE_PROFILE == "sit" ]; then
+    SKY_WALKING_BACKEND_ADDRESS="10.250.28.142:11800"
+    echo "skywalking backend_service_address: $SKY_WALKING_BACKEND_ADDRESS"
+elif [ $ACTIVE_PROFILE == "uat" ]; then
+    SKY_WALKING_BACKEND_ADDRESS="10.250.28.142:11800"
+    echo "skywalking backend_service_address: $SKY_WALKING_BACKEND_ADDRESS"
+elif [ $ACTIVE_PROFILE == "prod" ]; then
+    SKY_WALKING_BACKEND_ADDRESS="10.250.28.142:11800"
+    echo "skywalking backend_service_address: $SKY_WALKING_BACKEND_ADDRESS"
+fi
+SKY_WALKING_PARAM="-javaagent:$BASE_PACKAGE/agent/skywalking-agent.jar -Dskywalking.agent.service_name=$APP_NAME -Dskywalking.collector.backend_service=$SKY_WALKING_BACKEND_ADDRESS"
+SKY_WALKING_PLUGIN_PARAM="-Dskywalking.plugin.toolkit.log.grpc.reporter.server_host=10.250.28.142 -Dskywalking.plugin.toolkit.log.grpc.reporter.server_port=11800"
 
 JVM_PARAM_EXT="--spring.config.location=$BASE_PACKAGE/apps/$APP_NAME/config/"
 PIDS=`ps aux |grep [j]ava.*-Dspring.profiles.active=$ACTIVE_PROFILE.*$APP_NAME.*jar | awk {'print $2'}`
@@ -34,7 +49,7 @@ JAVA_EXEC=${JAVA_HOME}/bin/java
 # Java Executable - Jar Path Obtained from latest file in directory
 JAVA_APP=$(ls -t $BASE_PACKAGE/apps/$APP_NAME/$APP_NAME*.jar | head -n1)
 # To execute the application.
-FINAL_EXEC="$JAVA_EXEC $JVM_PARAM -jar $JAVA_APP $JVM_PARAM_EXT"
+FINAL_EXEC="$JAVA_EXEC $JVM_PARAM -jar $JAVA_APP $JVM_PARAM_EXT $SKY_WALKING_PARAM $SKY_WALKING_PLUGIN_PARAM"
 # Making executable command using tilde symbol and running completely detached from terminal
 `nohup $FINAL_EXEC  </dev/null >/dev/null 2>&1 &`
 #`nohup $FINAL_EXEC >> ../logs/$APP_NAME/$APP_NAME.log 2>&1 &`
