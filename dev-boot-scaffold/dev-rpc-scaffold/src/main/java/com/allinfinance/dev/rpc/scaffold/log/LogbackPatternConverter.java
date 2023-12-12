38df50aa8ff09ba8777bfe42066f9fca5afb3798
huanghf <huanghf@allinfinance.com>
2023-12-12 21:29:32 +0800

    feat(rpc-scaffold): 添加tid

---
+++ dev-boot-scaffold/dev-rpc-scaffold/src/main/java/com/allinfinance/dev/rpc/scaffold/log/LogbackPatternConverter.java
@@ -0,0 +1,23 @@
+package com.allinfinance.dev.rpc.scaffold.log;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author huanghf
+ * @date 2023/10/10 16:27
+ */
+public class LogbackPatternConverter extends ClassicConverter {
+    @Override
+    public String convert(ILoggingEvent event) {
+        StringBuilder tidBuilder = new StringBuilder("TID: ");
+        String tid = MDCUtils.getTid();
+        if (StringUtils.isNotBlank(tid)) {
+            tidBuilder.append(tid);
+        } else {
+            tidBuilder.append("N/A");
+        }
+        return tidBuilder.toString();
+    }
+}
---
+++ dev-boot-scaffold/dev-rpc-scaffold/src/main/java/com/allinfinance/dev/rpc/scaffold/log/TraceIdPatternLogbackLayout.java
@@ -0,0 +1,13 @@
+package com.allinfinance.dev.rpc.scaffold.log;
+
+import ch.qos.logback.classic.PatternLayout;
+
+/**
+ * @author huanghf
+ * @date 2023/10/10 16:25
+ */
+public class TraceIdPatternLogbackLayout extends PatternLayout {
+    static {
+        defaultConverterMap.put("tid", LogbackPatternConverter.class.getName());
+    }
+}
